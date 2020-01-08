b70746c705fa3321c152136e85ce997143722bff
Jonathan Joe <JonathanDonJoe@gmail.com>
2020-01-08 09:27:12 -0500

    Creates sanity check for MapsDisplay.  Adds mapData

---
+++ src/Components/MapsDisplay/MapsDisplay.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class MapsDisplay extends Component {
+    state = {
+
+    }
+    render() { 
+        return (
+            <div className='container'>
+                <h1>MapsDisplay</h1>
+            </div>
+        );
+    }
+}
+ 
+export default MapsDisplay;
