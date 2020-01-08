19f644282950f3758953228ac4c36e341a7d9617
Jonathan Joe <JonathanDonJoe@gmail.com>
2020-01-08 09:39:29 -0500

    Displays list of maps

--- src/Components/MapsDisplay/MapsDisplay.js
+++ src/Components/MapsDisplay/MapsDisplay.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 
+import './MapsDisplay.css';
+import mapData from '../../mapData.json';
+
 class MapsDisplay extends Component {
     state = {
 
     }
     render() { 
+        console.log(mapData.dictionary.maps);
+        console.log(mapData.dictionary);
+        let mapArray = []
+        let mapDataKeys = Object.keys(mapData.dictionary.maps);
+        console.log(mapDataKeys)
+        mapDataKeys.forEach( key => {
+            mapArray.push(mapData.dictionary.maps[key])
+        })
+        console.log(mapArray)
+        let mapItem = mapArray.map( (item, key) => <p key={key}>{item}</p>)
         return (
             <div className='container'>
-                <h1>MapsDisplay</h1>
+                <h1>Maps</h1>
+                {mapItem}
             </div>
         );
     }
