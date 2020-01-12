772bd8b95c4935d98b279521637d6e9d619d6ed5
Jonathan Joe <JonathanDonJoe@gmail.com>
2020-01-12 09:40:03 -0500

    Creates first map region filter

--- src/Components/MapsDisplay/MapsDisplay.js
+++ src/Components/MapsDisplay/MapsDisplay.js
@@ -6,12 +6,12 @@ import SingleMap from '../SingleMap/SingleMap';
 
 class MapsDisplay extends Component {
     state = {
-
+        filterBy: 'Lex Ejoris'
     }
 
     createMapComponentsArr = (mapDict) => {
         let mapDataKeys = Object.keys(mapDict);
-        let mapItems = mapDataKeys.map( (k, key) => 
+        let mapItems = mapDataKeys.filter(this.filterMaps).map( (k, key) => 
             // <div key={key}>
             //     <p>{mapData[k].name}</p>
             //     <p>{mapData[k].region}</p>
@@ -19,7 +19,13 @@ class MapsDisplay extends Component {
             // </div>
             < SingleMap key={key} k={key} mapData={mapData[k]} />
         )
+        // let mapArr =  mapDataKeys.map( key => mapData[k] )
         return mapItems
+    }
+
+    filterMaps = (mapKey) => {
+        return true;
+        // return mapData[mapKey].region === this.state.filterBy
     }
 
     render() {
