8dad04fc9faed3cb920863ebcd651f93643278e1
Jonathan Joe <JonathanDonJoe@gmail.com>
2020-01-14 13:42:31 -0500

    Adds region ids to mapData.json.  Creates filter checkboxes for regions.  Refactored filterBy state to log and change based on checkboxes

--- src/Components/MapsDisplay/MapsDisplay.js
+++ src/Components/MapsDisplay/MapsDisplay.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
 import './MapsDisplay.css';
-import mapData from '../../mapData.json';
+import { mapData, regions } from '../../mapData.json';
 import SingleMap from '../SingleMap/SingleMap';
 
 class MapsDisplay extends Component {
     state = {
-        filterBy: 'Lex Ejoris'
+        filterBy: [false, false, false, false, false, false, false, false]
     }
 
     createMapComponentsArr = (mapDict) => {
         let mapDataKeys = Object.keys(mapDict);
-        let mapItems = mapDataKeys.filter(this.filterMaps).map( (k, key) => 
+        let mapItems = mapDataKeys.filter(this.filterMaps).map((k, key) =>
             // <div key={key}>
             //     <p>{mapData[k].name}</p>
             //     <p>{mapData[k].region}</p>
@@ -22,6 +22,30 @@ class MapsDisplay extends Component {
         // let mapArr =  mapDataKeys.map( key => mapData[k] )
         return mapItems
     }
+    regionsCheckboxes = () => {
+        let checkboxes = Object.keys(regions).map((regionKey, k) =>
+            <div className='filter-checkbox-container-container' key={k}>
+                {regions[regionKey]}
+                <input type="checkbox" checked={this.state[k]} id={`region-${k}`} name={regions[regionKey]} onChange={this.toggleFilter} />
+            </div>
+        )
+        return <div className='filter-checkbox-container'>
+            {checkboxes}
+        </div>
+    }
+    toggleFilter = (e) => {
+        // console.log(e.target.name)
+        console.log(e.target.checked);
+        console.log(e.target.id);
+        console.log(e.target.id.slice(7));
+        let filterId = e.target.id.slice(7);
+        let newFilter = [...this.state.filterBy];
+        newFilter[filterId]= newFilter[filterId] ? false : true;
+        console.log(newFilter);
+        this.setState({
+            filterBy: newFilter
+        })
+    }
 
     filterMaps = (mapKey) => {
         return true;
@@ -30,11 +54,12 @@ class MapsDisplay extends Component {
 
     render() {
         // console.log(mapData);
-        
+
         return (
             <div className='container'>
                 <h1>Maps</h1>
-                <SingleMap mapData={{image_url:'', name: 'Name', region:'Region', tiers: ['Tiers']}} />
+                {this.regionsCheckboxes()}
+                <SingleMap mapData={{ image_url: '', name: 'Name', region: 'Region', tiers: ['Tiers'] }} />
                 {this.createMapComponentsArr(mapData)}
             </div>
         );
